7b49986cdcf1f807da51637e46973a8151e7cb82
Kiprich <davidkipkemoi30@gmail.com>
2023-07-13 19:12:35 +0300

    Daraja api

---
+++ server/controller/token.js
@@ -0,0 +1,47 @@
+const axios = require("axios");
+
+const createToken = async (req,res,next) =>{
+    const secret ="u5ADT2fYuOtJuJST";
+    const consumer = "y9yEPpZkle53rA8ATUMrCvssqZ06xwDK";
+    
+    let auth = new Buffer.from(`${consumer}:${secret}`).toString('base64')
+
+    await axios.get("https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials",
+        {
+            headers:{
+                authorization:`Basic ${auth}`
+            },
+
+        }
+    )
+    .then((data)=>{
+        console.log(data.data);
+        next()
+    })
+    .catch((err)=>{
+        console.log(err);
+        res.status(400).json(err.message);
+    })
+    };
+   const stkPush =async(req,res)=>{
+    const shortCode = 174379; 
+    const phone = req.body.phone.substring(1)
+    const amount =req.body.amount
+    const passkey = "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919"
+    const url = "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest"
+    
+    const date = new Date();
+    const timestamp = date.getFullYear() + 
+        ("0" + (date.getMonth() + 1)).slice(-2) +
+        ("0" + (date.getDate() + 1)).slice(-2) +
+        ("0" + (date.getHours() + 1)).slice(-2) +
+        ("0" + (date.getMinutes() + 1)).slice(-2) +
+        ("0" + (date.getSeconds() + 1)).slice(-2); 
+        const password = new Buffer.from(shortCode + passkey + timestamp).toString("base64")
+        const data={
+            
+        }
+   }
+   
+
+module.exports={createToken}
